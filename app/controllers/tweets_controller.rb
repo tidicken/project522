63089c28aac7329ef7b4efbd8a6053f7eb92b216
Ti Dickenson <ti@Tis-MacBook-Pro.local>
2014-05-22 10:08:38 -0400

    devise

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
+before_filter :authenticate_user!
   # GET /tweets
   # GET /tweets.json
   def index
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,6 @@
 module WelcomeHelper
+
+
+	
+
 end
