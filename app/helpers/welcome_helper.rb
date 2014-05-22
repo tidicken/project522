a8b4fe3d775fc08f1ce1d176f0bec11fc4fbac8a
Ti Dickenson <ti@Tis-MacBook-Pro.local>
2014-05-22 10:26:51 -0400

    name form done

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,16 @@
 module WelcomeHelper
 
 
-	
+	def resource_name
+    :tweet
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:tweet]
+  end
 
 end
